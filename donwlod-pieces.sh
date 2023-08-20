74dea1aa15bef146c167ecc20862a80250539036
devops <jlazaro@atsistemas.com>
2023-08-20 21:59:21 +0200

    Start

--- donwlod-pieces.sh
+++ donwlod-pieces.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 
-
-# mkdir data
-# cd data
+rm data -rf 2>/dev/null
+mkdir data
+cd data
 # curl --remote-name $1
 
-# Verifica si se proporcionó al menos un argumento
-if [ $# -lt 1 ]; then
-    echo "Uso: $0 <parametros>"
-    exit 1
-fi
 
 
 # Utiliza un bucle for para recorrer cada línea de los parámetros
-while IFS= read -r comando; do
-    # Ejecuta el comando
-    echo "Ejecutando comando: $comando"
-    # eval "$comando"
-done <<< "$1"
+#!/bin/bash
 
+archivo="datos.dat" # Nombre del archivo
+
+while IFS= read -r linea; do
+    # Realiza aquí la acción que desees con la línea
+    echo "Línea leída: $linea"
+    curl --remote-name $linea
+done < "$archivo"
 
 cd ..
