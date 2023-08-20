870faa7468a4b248445e55501dd4484e39404cd2
devops <jlazaro@atsistemas.com>
2023-08-20 12:48:24 +0200

    start

--- download-binary.sh
+++ download-binary.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
-./copia_job_recibido.sh user1 1234 user2 1234 folder_with_all_4
+rm -rf data 2>/dev/null
+
+
+# try{
+#     sh "docker rm -f clamav "
+# } catch(Exception e) {
+#     echo "Error al eliminar el contenedor clamav: $e"
+# }
+
+try{
+    sh "docker volume rm nombre-volumen"
+} catch(Exception e) {
+    echo "Error al eliminar el volumen: $e"
+}
+
+cd data
+curl --remote-name $1
+
+# sh "echo 'datos del modelo'>datos.dat"
+chmod -R a+r .
+# sh "chmod -R a+r \$(pwd)/datos.dat"
+docker volume create clamav-volumen
+docker run --name clamav -d -v clamav-volumen:/data clamav/clamav
+docker cp * clamav:/data
+# sh "docker exec clamav clamscan -r /data >result.txt"
+# sh "echo ============================================================"
+# sh "cat result.txt"
+# sh "echo ============================================================"
+
+#  try{
+#     sh "docker rm -f clamav "
+# } catch(Exception e) {
+#     echo "Error al eliminar el contenedor clamav: $e"
+# }
+
+# try{
+#     sh "docker volume rm nombre-volumen"
+# } catch(Exception e) {
+#     echo "Error al eliminar el volumen: $e"
+# }
+
