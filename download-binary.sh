6f0965629a23a73581e2410cdc595aaf0bc4801f
devops <jlazaro@atsistemas.com>
2023-08-20 14:26:44 +0200

    Start

--- download-binary.sh
+++ download-binary.sh
@@ -8,8 +8,10 @@ mkdir data
 cd data
 curl --remote-name $1
 
+# see https://docs.docker.com/engine/security/trust/
+export DOCKER_CONTENT_TRUST=1
+
 chmod -R a+r .
-# sh "chmod -R a+r \$(pwd)/datos.dat"
 docker volume create clamav-volumen
 docker run --name clamav -d -v clamav-volumen:/data clamav/clamav
 docker cp * clamav:/data
@@ -20,8 +22,13 @@ cat result.txt
 echo ============================================================
 
 docker rm -f clamav
-docker volume rm nombre-volumen
+docker volume rm clamav-volumen
+
+# see https://docs.docker.com/engine/security/trust/
+export DOCKER_CONTENT_TRUST=0
 
 cd ..
 cd ..
 rm -rf download-models 2>/dev/null
+
+
